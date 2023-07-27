d1f3a9319dabb7dd96119477cdbaeeea88b4e554
ChenEylon <cheneylon1@gmail.com>
2023-07-27 16:42:12 +0300

    commit2

--- models/payment.js
+++ models/payment.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const payment = new Schema({
+    parkName:{type:String, required: true},
     startTime: {type:String, required: true},
     endTime: {type:String},
     pricePerHour: {type:String, required: true},  
     parkingLocation: {type:String, required: true},
     phoneToPay: {type:String, required: true},
+    date: {type: Date, default: Date.now,}
 })
 
 module.exports=mongoose.model("payment", payment)
