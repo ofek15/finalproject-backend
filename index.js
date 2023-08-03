598452c1e731ad618b9cc465ef78b6689f67d4c4
amitbarha <amitbar131@gmail.com>
2023-08-03 14:52:47 +0300

    gg

--- index.js
+++ index.js
@@ -16,13 +16,10 @@ mongoose
   .catch((err) => console.log(err.message));
 
   app.use(cors())
-  app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-    });
+  app.use((req, res, next) => {
+    res.set("Access-Control-Allow-Origin", "http://localhost:5000")
+    res.json({ msg: "This is CORS-enabled for a Single Route" })
+  })
   
 
 app.get("/", (req, res) => {
